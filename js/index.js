9ee26d067d62eea587caef53467f705aaf7b22a1
Amitabh Shahi <amitabhshahi09@gmail.com>
2024-11-22 19:16:34 +0530

    initiated cart method

--- js/index.js
+++ js/index.js
@@ -24,18 +24,28 @@ const dishArray = [
     { category :'South Indian',food: [{name:'Pizza',price:799,path:'./images/pizza.jpg', desc:'lorem ipsum', quantity:0},{name:'Burger',price:129,path:'./images/burger.jpg', desc:'lorem ipsum', quantity:0},{name:'Burger',price:129,path:'./images/burger.jpg', desc:'lorem ipsum', quantity:0},{name:'Burger',price:129,path:'./images/burger.jpg', desc:'lorem ipsum', quantity:0},{name:'Burger',price:129,path:'./images/burger.jpg', desc:'lorem ipsum', quantity:0}] }
 ];
 
+displayDish(dishArray)
 function displayDish(arr) {
     let str = ``;
     for(item of arr) {
         str += `<div>`;
         str += `<h2 class="sec3Caterory">Explore the varities of ${item.category} Dishes </h2> <div class="gridSB30px"> `;
         for(i of item.food) {
-            str += `<div class="card columnFlexSBC"> <div><img src=${i.path} alt="noImage"><h3>${i.name}</h3></div> <p>${i.desc}</p> <p>${i.price}</p> <div class="rowFlexSBC"><p>Quantity</p><p>${i.quantity}</p></div> <button id="addToCart">Add To Cart</button></div>`;
+            str += `<div class="card columnFlexSBC"> <div class="imgDiv"><img src=${i.path} alt="noImage"></div> <h3>${i.name}</h3> <p>${i.desc}</p> <p>₹${i.price}</p> <div id='quantityDiv' class="rowFlexSBC"><p>Quantity</p><p>${i.quantity}</p></div> <button id="addToCart" >Add To Cart</button></div>`;
         }
         str += `</div></div>`;
     }
-    return str;
+    
+    document.querySelector('.sec3').innerHTML = str;
+
+    let addToCart = document.querySelectorAll('#addToCart');
+    let quantityDiv = document.querySelectorAll('#quantityDiv');
+    addToCart.addEventListener("click", addToCartFunction);
+    function addToCartFunction() {
+        console.log('hello mc');
+        quantityDiv[0].style.visibility = "visible";
+    }
 }
-document.querySelector('.sec3').innerHTML = displayDish(dishArray);
+
 // *******************************************************************************************************************************//
 
